9e2f9a42cc2f96163bf616902009d634f34f47ed
Ukasyah <hi.ukasyah@gmail.com>
2025-12-17 21:28:43 +0700

    feat: allow to push schema to remote db

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,24 @@
 import "dotenv/config";
-import { defineConfig } from "drizzle-kit";
+import { Config, defineConfig } from "drizzle-kit";
 
-export default defineConfig({
+let config: Config = {
   dialect: "sqlite",
   schema: "./app/db/schema.ts",
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
-});
+};
+
+if (process.env.CLOUDFLARE_ACCOUNT_ID) {
+  config = {
+    ...config,
+    driver: "d1-http",
+    dbCredentials: {
+      accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+      databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+      token: process.env.CLOUDFLARE_TOKEN!,
+    },
+  };
+}
+
+export default defineConfig(config);
